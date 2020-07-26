f66fa225ce72c7ab8a5e9fbc0fc4b1492b83d6b2
allentom <qweasdzxczse2@gmail.com>
2020-07-26 23:08:27 +0900

    add:[tag detail] empty view

--- lib/pages/tag/tag.dart
+++ lib/pages/tag/tag.dart
@@ -4,19 +4,26 @@ import 'package:provider/provider.dart';
 import 'package:youcomic/api/model/tag_entity.dart';
 import 'package:youcomic/components/book_info_bottom_sheet.dart';
 import 'package:youcomic/components/book_item.dart';
+import 'package:youcomic/components/empty_view.dart';
 import 'package:youcomic/config/application.dart';
 import 'package:youcomic/pages/tag/menu.dart';
 import 'package:youcomic/pages/tag/provider.dart';
 
 class TagPage extends StatelessWidget {
   final TagEntity tagEntity;
+
   TagPage({this.tagEntity});
+
   static launch(BuildContext context, TagEntity tag) {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => TagPage(tagEntity: tag,)),
+      MaterialPageRoute(
+          builder: (context) => TagPage(
+                tagEntity: tag,
+              )),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<TagProvider>(
@@ -27,21 +34,29 @@ class TagPage extends StatelessWidget {
         Future _pullToRefresh() async {
           await tagProvider.bookDataSource.loadBooks(true);
         }
+
         List<Widget> items = [];
-        createBookItem(book){
-          items.add(BookItem(book:book,onLongPress: () {
-            HapticFeedback.vibrate();
-            showModalBottomSheet(
-                context: context,
-                builder: (context) {
-                  return BookInfoBottomSheet(
-                    bookEntity: book,
-                  );
-                });
-          },));
-          items.add(Divider(height: 0,));
+        createBookItem(book) {
+          items.add(BookItem(
+            book: book,
+            onLongPress: () {
+              HapticFeedback.vibrate();
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return BookInfoBottomSheet(
+                      bookEntity: book,
+                    );
+                  });
+            },
+          ));
+          items.add(Divider(
+            height: 0,
+          ));
         }
-        tagProvider.bookDataSource.books.forEach((book) => createBookItem(book));
+
+        tagProvider.bookDataSource.books
+            .forEach((book) => createBookItem(book));
 
         ScrollController _controller = new ScrollController();
         _controller.addListener(() {
@@ -49,10 +64,24 @@ class TagPage extends StatelessWidget {
           var pixel = _controller.position.pixels;
           if (maxScroll == pixel) {
             tagProvider.loadMoreBooks();
-          } else {
-          }
+          } else {}
         });
-
+        Widget list = ListView(
+          children: items.toList(),
+          controller: _controller,
+        );
+        Widget emptyView = EmptyView(
+          isLoading:  tagProvider.bookDataSource.isLoading,
+          icon: Icon(
+            Icons.bookmark,
+            size: 96,
+            color: Colors.black26,
+          ),
+          text: "暂时没有订阅的标签",
+          onRefresh: (){
+          tagProvider.bookDataSource.loadBooks(true);
+          },
+        );
         return Scaffold(
           appBar: AppBar(
             brightness: Brightness.light,
@@ -66,13 +95,7 @@ class TagPage extends StatelessWidget {
               style: TextStyle(color: Colors.black87),
             ),
           ),
-          body: RefreshIndicator(
-            onRefresh: _pullToRefresh,
-            child: ListView(
-              children: items.toList(),
-              controller: _controller,
-            ),
-          ),
+          body: tagProvider.bookDataSource.books.isEmpty?emptyView:list,
         );
       }),
     );
